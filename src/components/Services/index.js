580114ca86d70f3a7f163cbd1455d02228a2871c
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-12-31 13:27:25 -0700

    Add map to display Services from json

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Icon1 from '../../images/svg-1.svg';
-import Icon2 from '../../images/svg-2.svg';
-import Icon3 from '../../images/svg-3.svg';
+import React, { useState, useEffect } from 'react';
+// import Icon1 from '../../images/svg-1.svg';
+// import Icon2 from '../../images/svg-2.svg';
+// import Icon3 from '../../images/svg-3.svg';
 import {
   ServicesContainer,
   ServicesH1,
@@ -13,33 +13,26 @@ import {
 } from './ServicesElements';
 
 const Services = () => {
+  const [services, setServices] = useState(() => []);
+
+  useEffect(() => {
+    fetch('./JSON/services.json')
+      .then((response) => response.json())
+      .then((result) => setServices(result));
+  }, []);
+
   return (
     <ServicesContainer id='services'>
       <ServicesH1>Our Services</ServicesH1>
       <ServicesWrapper>
-        <ServicesCard>
-          <ServicesIcon src={Icon1} />
-          <ServicesH2>
-            Reduce expenses Reduce expensesRe duce expensesRe duce expe nses
-          </ServicesH2>
-          <ServicesP>
-            We help qwoeifjqw oeifjqow eifjqwoef ijqwoei fjqwoef...
-          </ServicesP>
-        </ServicesCard>
-        <ServicesCard>
-          <ServicesIcon src={Icon2} />
-          <ServicesH2>Reduce expenses</ServicesH2>
-          <ServicesP>
-            Wee help qwoeifjqwoe ifjqoweif jqwoe fijqwoeifjqw oef help...
-          </ServicesP>
-        </ServicesCard>
-        <ServicesCard>
-          <ServicesIcon src={Icon3} />
-          <ServicesH2>Reduce expenses</ServicesH2>
-          <ServicesP>
-            We e help qwoeifjqw oeifjqow eifjq oefijqw oeif jqwoef help...
-          </ServicesP>
-        </ServicesCard>
+        {services.map((service, key) => (
+          <ServicesCard key={key}>
+            <ServicesIcon src={require(`../../images/${service.icon}.svg`)} />
+            {/* <ServicesIcon src={require(`../../images/${service.icon}.svg`)} /> */}
+            <ServicesH2>{service.title}</ServicesH2>
+            <ServicesP>{service.text}</ServicesP>
+          </ServicesCard>
+        ))}
       </ServicesWrapper>
     </ServicesContainer>
   );
