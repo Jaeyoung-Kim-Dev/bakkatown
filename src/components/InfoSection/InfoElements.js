d34731a7a9c8f6c5be8d0cb96627249d3cbb546d
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2021-01-01 22:09:59 -0700

    Revert "Revert "Change components height from pixel to vh""
    This reverts commit 7ff66f01be1372075f411e86e2414a017478d626.

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -1,8 +1,23 @@
 import styled from 'styled-components';
+import { Link } from 'react-scroll';
+
+const backgroundDark = '#010606';
+const backgroundLight = '#f9f9f9';
+const topLine = '#01bf71';
+const headingDark = '#f7f8fa';
+const headingLight = '#010606';
+const subtitleDark = '#fff';
+const subtitleLight = '#010606';
+const btnBgDark = '#01BF71';
+const btnBgLight = '#010606';
+const btnTxtDark = '#010606';
+const btnTxtLight = '#fff';
+const btnBgHoverDark = '#fff';
+const btnBgHoverLight = '#01BF71';
 
 export const InfoContainer = styled.div`
   color: #fff;
-  background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#010606')};
+  background: ${({ isDark }) => (isDark ? backgroundDark : backgroundLight)};
 
   @media screen and (max-width: 768px) {
     padding: 100px 0;
@@ -12,7 +27,7 @@ export const InfoContainer = styled.div`
 export const InfoWrapper = styled.div`
   display: grid;
   z-index: 1;
-  height: 860px;
+  height: 90vh;
   width: 100%;
   max-width: 1100px;
   margin-right: auto;
@@ -53,7 +68,7 @@ export const TextWrapper = styled.div`
 `;
 
 export const TopLine = styled.p`
-  color: #01bf71;
+  color: ${topLine};
   font-size: 16px;
   line-height: 16px;
   font-weight: 700;
@@ -67,7 +82,7 @@ export const Heading = styled.h1`
   font-size: 48px;
   line-height: 1.1;
   font-weight: 600;
-  color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#010606')};
+  color: ${({ isDark }) => (isDark ? headingDark : headingLight)};
 
   @media screen and (max-width: 480px) {
     font-size: 32px;
@@ -79,12 +94,33 @@ export const Subtitle = styled.p`
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 24px;
-  color: ${({ darkText }) => (darkText ? '#010606' : '#fff')};
+  color: ${({ isDark }) => (isDark ? subtitleDark : subtitleLight)};
 `;
 
 export const BtnWrap = styled.div`
   display: flex;
   justify-content: flex-start;
+`;
+
+export const Button = styled(Link)`
+  border-radius: 50px;
+  background: ${({ isDark }) => (isDark ? btnBgDark : btnBgLight)};
+  white-space: nowrap;
+  padding: 12px 30px;
+  color: ${({ isDark }) => (isDark ? btnTxtDark : btnTxtLight)};
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: ${({ isDark }) => (isDark ? btnBgHoverDark : btnBgHoverLight)};
+  }
 `;
 
 export const ImgWrap = styled.div`
