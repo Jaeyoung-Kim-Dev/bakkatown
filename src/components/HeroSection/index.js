7796abfeada939cabd746540111d19ad2bfec78d
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-01-09 01:26:30 -0700

    animation bug fix

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { TimelineMax, Power2 } from 'gsap';
+import { gsap, Power2 } from 'gsap';
 import {
   HeroContainer,
   HeroContent,
@@ -24,22 +24,30 @@ const HeroSection = () => {
   let slider = useRef();
 
   useEffect(() => {
-    let tl = new TimelineMax();
-    tl.from(content.current, {
+    gsap.from(content.current, {
       duration: 1,
+      autoAlpha: 0,
+      ease: 'none',
       height: '0%',
-      ease: Power2.easeInOut,
-    })
-      .from(content.current, {
-        duration: 1.2,
-        width: '100%',
-        ease: Power2.easeInOut,
-      })
-      .from(
-        slider.current,
-        { duration: 1.2, x: '-100%', ease: Power2.easeInOut },
-        '-=1.2'
-      );
+      delay: Power2.easeInOut,
+    });
+    // let tl = new TimelineMax();
+    // gsap
+    //   .from(content.current, {
+    //     duration: 1,
+    //     height: '0%',
+    //     ease: Power2.easeInOut,
+    //   })
+    //   .from(content.current, {
+    //     duration: 1.2,
+    //     width: '100%',
+    //     ease: Power2.easeInOut,
+    //   })
+    //   .from(
+    //     slider.current,
+    //     { duration: 1.2, x: '-100%', ease: Power2.easeInOut },
+    //     '-=1.2'
+    //   );
   }, []);
 
   return (
