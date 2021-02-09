0a9998c2276c16d353f80c0a694b19946ccbcf33
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2021-02-08 23:06:27 -0700

    Use Material UI to Booking page

--- src/components/Book/Availability.js
+++ src/components/Book/Availability.js
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import {
-  FormContent,
-  FormH1,
-  FormLabel,
-  FormInput,
-  FormSelect,
-} from './BookElements';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { FormContent, FormH1, FormInput } from './BookElements';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const Availability = (props) => {
   const [dateRange, setDateRange] = useState([
@@ -18,6 +28,8 @@ const Availability = (props) => {
       key: 'selection',
     },
   ]);
+
+  const classes = useStyles();
 
   function dateHandleChange(ranges) {
     setDateRange(ranges);
@@ -31,7 +43,7 @@ const Availability = (props) => {
   return (
     <>
       <FormContent>
-        <FormH1>Date</FormH1>
+        <FormH1>AVAILABILITY</FormH1>
         <DateRange
           onChange={(item) => dateHandleChange([item.selection])}
           showSelectionPreview={true}
@@ -41,30 +53,34 @@ const Availability = (props) => {
           minDate={new Date()}
           direction='horizontal'
         />
-        <FormLabel htmlFor='guests'>Guests</FormLabel>
-        <FormSelect
-          name='guests'
-          onChange={props.handleChange}
-          placeholder='2'
-          defaultValue={2}
-          required
-        >
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-          <option value={6}>6</option>
-          <option value={7}>7</option>
-        </FormSelect>
-        <FormInput
-          type='text'
-          id='promoCode'
-          name='promoCode'
-          placeholder='Promotion / Group Code'
-          onChange={props.handleChange}
-          required
-        />
+        <FormControl className={classes.formControl}>
+          <InputLabel shrink id='demo-simple-select-placeholder-label-label'>
+            Guests
+          </InputLabel>
+          <Select
+            labelId='demo-simple-select-placeholder-label-label'
+            id='demo-simple-select-placeholder-label'
+            name='guests'
+            value={props.booking.guests}
+            onChange={props.handleChange}
+            displayEmpty
+            className={classes.selectEmpty}
+          >
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={6}>6</MenuItem>
+            <MenuItem value={7}>7</MenuItem>
+          </Select>
+          <br />
+          <TextField
+            id='standard-secondary'
+            label='Promotion / Group Code'
+            color='secondary'
+          />
+        </FormControl>
       </FormContent>
     </>
   );
