2d5998d59f28cb35dd42872168aa8c1f7b53b556
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-03-20 02:22:43 -0600

    Partial making booking Redux works

--- src/components/Rooms/index.js
+++ src/components/Rooms/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import store from '../../store';
 import axios from 'axios';
 import { BiUser, BiBed, BiBath, BiWifi } from 'react-icons/bi';
 // import RoomLists from './roomLists.json';
@@ -17,83 +18,98 @@ import {
 } from './RoomsElements';
 
 const Rooms = () => {
-  const [roomLists, setRoomLists] = useState([]);
+  const [roomLists, setRoomLists] = useState(store.getState().roomLists);
+
+  store.subscribe(() => setRoomLists(store.getState().roomLists));
 
   useEffect(() => {
-    axios.get(`http://localhost:8080/room`).then((res) => {
-      setRoomLists(res.data);
-    });
+    store.dispatch({ type: 'fetchRooms' });
+    // axios.get(`http://localhost:8080/room`).then((res) => {
+    //   setRoomLists(res.data);
+    // });
   }, []);
+  console.log({ roomLists });
 
   return (
     <RoomsContainer id='services'>
       <RoomsH1>Our Rooms</RoomsH1>
-      <RoomsWrapper>
-        {roomLists.map((room) => (
-          <RoomsCard key={room.roomTypeId}>
-            <RoomsH2>{room.roomTitle}</RoomsH2>
-            <RoomsImage
-              src={require(`../../images/rooms/${room.roomTitle}.jpg`)?.default}
-              alt={room.roomTitle}
-            />
-            {/* ?.default is temporary because of react-scripts v4.0.1's bug */}
+      {/* <button onClick={() => store.dispatch({ type: 'tt', newVal: 10 })}>
+        e
+      </button> */}
+      <button onClick={() => store.dispatch({ type: 'fetchRooms' })}>e</button>
+      {roomLists ? (
+        <RoomsWrapper>
+          {roomLists.map((room) => (
+            <RoomsCard key={room.roomTypeId}>
+              <RoomsH2>{room.roomTitle}</RoomsH2>
+              <RoomsImage
+                src={
+                  require(`../../images/rooms/${room.roomTitle}.jpg`)?.default
+                }
+                alt={room.roomTitle}
+              />
+              {/* ?.default is temporary because of react-scripts v4.0.1's bug */}
 
-            <RoomSpecs>
-              <RoomSpecList>
-                <BiUser
-                  style={{
-                    color: 'green',
-                    marginRight: '0.2rem',
-                  }}
-                />
-                {room.roomCapacity}
-              </RoomSpecList>
-              <RoomSpecList>
-                <BiBed
-                  style={{
-                    color: 'green',
-                    marginRight: '0.2rem',
-                  }}
-                />
-                {room.bedCount}
-              </RoomSpecList>
-              <RoomSpecList>
-                <BiBath
-                  style={{
-                    color: 'green',
-                    marginRight: '0.2rem',
-                  }}
-                />
-                {room.bathCount}
-              </RoomSpecList>
-              <RoomSpecList>
-                {room.size}
-                <span
-                  style={{
-                    fontSize: '0.7rem',
-                    color: 'green',
-                    marginLeft: '0.1rem',
-                  }}
-                >
-                  SQF
-                </span>
-              </RoomSpecList>
-              {room.wifi && (
+              <RoomSpecs>
                 <RoomSpecList>
-                  <BiWifi />
+                  <BiUser
+                    style={{
+                      color: 'green',
+                      marginRight: '0.2rem',
+                    }}
+                  />
+                  {room.roomCapacity}
                 </RoomSpecList>
-              )}
-            </RoomSpecs>
-            <RoomsP>
-              From <span style={{ fontSize: '1.7rem' }}>${room.roomCost}</span>{' '}
-              Per Night
-            </RoomsP>
-            <BtnWrap>
-              <Button>More Info</Button>
-            </BtnWrap>
-          </RoomsCard>
-        ))}
-      </RoomsWrapper>
+                <RoomSpecList>
+                  <BiBed
+                    style={{
+                      color: 'green',
+                      marginRight: '0.2rem',
+                    }}
+                  />
+                  {room.bedCount}
+                </RoomSpecList>
+                <RoomSpecList>
+                  <BiBath
+                    style={{
+                      color: 'green',
+                      marginRight: '0.2rem',
+                    }}
+                  />
+                  {room.bathCount}
+                </RoomSpecList>
+                <RoomSpecList>
+                  {room.size}
+                  <span
+                    style={{
+                      fontSize: '0.7rem',
+                      color: 'green',
+                      marginLeft: '0.1rem',
+                    }}
+                  >
+                    SQF
+                  </span>
+                </RoomSpecList>
+                {room.wifi && (
+                  <RoomSpecList>
+                    <BiWifi />
+                  </RoomSpecList>
+                )}
+              </RoomSpecs>
+              <RoomsP>
+                From{' '}
+                <span style={{ fontSize: '1.7rem' }}>${room.roomCost}</span> Per
+                Night
+              </RoomsP>
+              <BtnWrap>
+                <Button>More Info</Button>
+              </BtnWrap>
+            </RoomsCard>
+          ))}
+        </RoomsWrapper>
+      ) : (
+        <button>nothing</button>
+      )}
     </RoomsContainer>
   );
 };
