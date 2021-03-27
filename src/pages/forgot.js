8687b987da0e02f349b3413dab33ae997299ff95
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-03-26 23:33:39 -0600

    combine pages

---
+++ src/pages/forgot.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Forgot from '../components/Account/Forgot';
+import ScrollToTop from '../components/ScrollToTop';
+
+const ForgotPage = () => {
+    return (
+        <>
+            <ScrollToTop />
+            <Forgot />
+        </>
+    );
+};
+
+export default ForgotPage;
