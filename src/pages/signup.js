ccbbac95ed4a6d263c8c0d2e8ff9dfd7014b0f67
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-03-29 03:27:47 -0600

    Add navbar to the signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 import SignUp from '../components/Account/Signup';
 import ScrollToTop from '../components/ScrollToTop';
 
 const SignupPage = () => {
-    return (
-        <>
-            <ScrollToTop />
-            <SignUp />
-        </>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <SignUp />
+    </>
+  );
 };
 
 export default SignupPage;
